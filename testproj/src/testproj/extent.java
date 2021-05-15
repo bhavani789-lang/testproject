9c94e3f2549560bb7097c55522a3090b07e05ed8
BHAVANI <bhavanarani797@gmail.com>
2021-05-15 11:57:55 +0530

    new changes added to project

--- testproj/src/testproj/extent.java
+++ testproj/src/testproj/extent.java
@@ -1,5 +1,13 @@
 package testproj;
 
 public class extent {
-
+	public int m1(int a,int b)
+	{
+		int c =a+b;
+		return c;
+	}
+	public static void main(String[] args) {
+		extent ad = new extent();
+		ad.m1(20, 30);
+	}
 }
